6dff86de8e866a0ec5d5a318f7c8d4c1562cce91
egorprh <45630690+egorprh@users.noreply.github.com>
2025-05-13 14:25:21 +0300

    Add files via upload

---
+++ assets/Alert-CRSVFpEW.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D8uIVFAW.js";function t({text:s}){return r.jsx("div",{className:"save-error",children:r.jsxs("p",{children:["⚠️ ",s]})})}export{t as A};
---
+++ assets/ContentNotFound-BGXjiI8p.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-D8uIVFAW.js";function s({message:o}){return n.jsx("div",{className:"content-not-found",children:n.jsx("p",{className:"not-found-message",children:o})})}export{s as C};
